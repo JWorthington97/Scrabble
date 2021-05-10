1cb4cb465c20f1d3183349bd1c37879f808acbb7
JWorthington97 <jb.worthington@live.co.uk>
2021-05-10 20:27:22 +0100

    words onto board

--- board_functions.py
+++ board_functions.py
@@ -3,14 +3,53 @@ def create_scrabble_board():
     # double spaces here because of the 2*
     array = []
     for i in range(15):
-        array.append(['   ']*15)
+        array.append([' ']*15)
     return array
 
 def show_scrabble_board(board):
-    print('A  B  C  D  E  F  G  H  I  J  K  L  M  N  O')
+    print('    A B C D E F G H I J K L M N O')
     # Need to align the top row with the board (e.g double digits too)
     row_index = 0
     for row in board:
-       # print(row)
-        #print(str(row_index) + ''.join(row))
+        if row_index < 10:
+           y = ' ' + str(row_index) + '  '
+        else:
+            y = str(row_index) + '  '
+        
+        print(str(y) + ' '.join(row))
         row_index += 1
+
+def add_word_to_board(proposed_word, proposed_location, proposed_direction,board):
+    #word has already been checked
+    #word checks 
+    word = proposed_word.upper()
+    if word.isalpha() == False:
+        return -1 
+    #location checks
+
+    if int(proposed_location[1]) < 0 or int(proposed_location[1]) > 14:
+        return -2
+    if proposed_location[0].upper() < 'A' or proposed_location[0].upper() > 'O':
+        return -2
+    #direction checks
+    direction = proposed_direction.upper()
+    if direction not in ['R', 'D',  'RIGHT', 'DOWN']:
+        return -3
+
+    #convert character into integer
+    column = ord(proposed_location[0]) - 65 # ASCII value for 'A' + 1 because of board quirks
+    row = int(proposed_location[1])
+    if direction in ['R', 'RIGHT']:
+        if column + len(word) >= 16:
+            return -4
+        else:
+            for index, letter in enumerate(word):
+                board[row][column + index] = letter
+    else:
+        if row + len(word) >= 16:
+            return -4
+        else:
+            for index, letter in enumerate(word):
+                board[row + index][column] = letter
+    return 0
+    
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from board_functions import *
 from bag_functions import *
 from player_functions import *
 from word_functions import *
-
+import pprint
 
 # Initialises empty board
 board = create_scrabble_board()
@@ -37,12 +37,54 @@ board[10][7] ='N'
 
 board[3][5] = '*'
 
+board[6][8] = 'O'
+board[6][9] = 'V'
+board[6][10] = 'E'
+
+board[8][5] = 'S'
+board[8][6] = 'A'
+
+board[3][6] = 'T'
+
+board[7][8] = 'T'
+board[8][8] = 'H'
+board[9][8] = 'E'
+board[10][8] = 'R'
+
 show_scrabble_board(board)
+
+# receive word, location, direction
+# find all the collisions on the board, and save them all as words
+# check all of the words
+#if ONE is not a word, can't put the received word down
+# 6              L O V E
+# 7              E T
+# 8          S A M H
+# 9              O E
+#10              N R
+
+#function to get array of collision words
+#function to check them all (already got)
+#function to add to board
+#function to check proposed word, direction, location not impeding another word
 
 
 # * = 2* letter
 # ^ = 3* letter
 # £ = 2* word
 
+# proposed_word = input("Provide word: ")
+# proposed_location = input("Location: ")
+# proposed_direction = input("Direction (Up, Right, Down): ")
+proposed_word = 'tortoise'
+proposed_location = 'H1'
+proposed_direction = 'Right'
+word_check = add_word_to_board(proposed_word, proposed_location, proposed_direction,board)
+if word_check == -1:
+    print("Word is invalid. Please ensure you only use characters.")
+# need to add while loop/try/catch etc for when when the function returns -1, -2, -3, or -4
+#while add_word_to_board(proposed_word, proposed_location, proposed_direction,board) != 0:
+    
+show_scrabble_board(board)
 
 
